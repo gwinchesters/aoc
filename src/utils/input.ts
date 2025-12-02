9891b5624a38ab4cfa23ded2a53949e7efb2c786
George Smith <george@ndustrial.io>
2025-12-02 08:46:46 -0500

    feat: alt input parsing func

--- src/utils/input.ts
+++ src/utils/input.ts
@@ -6,3 +6,19 @@ export const parseInput = (file: string): string[] => {
 
   return input.split('\n')
 }
+
+export function parse<T>(
+  file: string,
+  splitOn: string,
+  parseLine: (line: string) => T | undefined,
+): T[] {
+  const input = readFileSync(resolve(__dirname, `../input/${file}.txt`), 'utf8')
+  const lines = input.split(splitOn)
+
+  return lines.reduce<T[]>((acc, line) => {
+    const t = parseLine(line)
+    if (!t) return acc
+    acc.push(t)
+    return acc
+  }, [] as T[])
+}
